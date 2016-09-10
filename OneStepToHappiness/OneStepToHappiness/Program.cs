ee8c2a536ae42206006f6ee6514adb5d1b33487a
egorsh0 <egorsh@inbox.ru>
2016-09-10 17:54:20 +0300

    Изменено название переменных

--- OneStepToHappiness/OneStepToHappiness/Program.cs
+++ OneStepToHappiness/OneStepToHappiness/Program.cs
@@ -13,7 +13,7 @@ namespace OneStepToHappiness
             string _number = Console.ReadLine();
             int fullNumber = Convert.ToInt32(_number);
             int low = fullNumber - 1;
-            int hight = fullNumber + 1;
+            int high = fullNumber + 1;
 
             string sLow = Convert.ToString(low);
             int lLength = sLow.Length;
@@ -26,20 +26,20 @@ namespace OneStepToHappiness
                 return;
             }
 
-            string sHight = Convert.ToString(hight);
-            int lHight = sHight.Length;
-            if (lHight < 6)
-                for (int i = 0; i < 6 - lHight; i++)
-                    sHight = "0" + sHight;
+            string sHigh = Convert.ToString(high);
+            int lHigh = sHigh.Length;
+            if (lHigh < 6)
+                for (int i = 0; i < 6 - lHigh; i++)
+                    sHigh = "0" + sHigh;
 
             char[] _low = sLow.ToCharArray();
-            char[] _hight = sHight.ToCharArray();
+            char[] _high = sHigh.ToCharArray();
 
             int lFirst = Convert.ToInt32(_low[0]) + Convert.ToInt32(_low[1]) + Convert.ToInt32(_low[2]);
             int lSecond = Convert.ToInt32(_low[3]) + Convert.ToInt32(_low[4]) + Convert.ToInt32(_low[5]);
        
-            int hFirst = Convert.ToInt32(_hight[0]) + Convert.ToInt32(_hight[1]) + Convert.ToInt32(_hight[2]);
-            int hSecond = Convert.ToInt32(_hight[3]) + Convert.ToInt32(_hight[4]) + Convert.ToInt32(_hight[5]);
+            int hFirst = Convert.ToInt32(_high[0]) + Convert.ToInt32(_high[1]) + Convert.ToInt32(_high[2]);
+            int hSecond = Convert.ToInt32(_high[3]) + Convert.ToInt32(_high[4]) + Convert.ToInt32(_high[5]);
 
             if ((lFirst == lSecond) || (hFirst == hSecond))
                 Console.WriteLine("Yes");
