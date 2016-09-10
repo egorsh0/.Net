641bc0485036a95d87c4679f921c68804a77938e
egorsh0 <egorsh@inbox.ru>
2016-09-10 15:29:50 +0300

    Решение задачи 1636. Штрафное время

---
+++ TimePenalty/TimePenalty/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimePenalty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] timeTeams = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int ZZZ = Convert.ToInt32(timeTeams[0]);
+            int QXX = Convert.ToInt32(timeTeams[1]);
+            string[] time = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int sum = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                sum = sum + Convert.ToInt32(time[i]);
+            }
+            if (ZZZ + (sum * 20) > QXX)
+                Console.WriteLine("Dirty debug :(");
+            else
+                Console.WriteLine("No chance.");
+        }
+    }
+}
---
+++ TimePenalty/TimePenalty/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TimePenalty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimePenalty")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f6d5cc2b-3044-4de9-bb2f-bf867fac42c5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
