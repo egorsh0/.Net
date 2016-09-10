e2ca03e6a5b21ecb8acac7ccd4b6ab224aaa2b21
egorsh0 <egorsh@inbox.ru>
2016-09-10 14:37:06 +0300

    Первый тестируемый вариант задачи 1607

---
+++ Taxi/Taxi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int moneyPetya = Convert.ToInt32(data[0]);
+            int moneyTaxi = Convert.ToInt32(data[2]);
+            int maxTaxi = moneyTaxi;
+            bool isAgree = false;
+            while(isAgree != true)
+            {
+                moneyPetya = moneyPetya + Convert.ToInt32(data[1]);
+                if (moneyPetya > moneyTaxi)
+                {
+                    isAgree = true;
+                    break;
+                }
+                else
+                {
+                    moneyTaxi = moneyTaxi - Convert.ToInt32(data[3]);
+                    if (moneyTaxi != moneyPetya)
+                    {
+                        if (moneyTaxi < moneyPetya)
+                        {
+                            moneyTaxi = moneyPetya;
+                            isAgree = true;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(moneyPetya);
+        }
+    }
+}
