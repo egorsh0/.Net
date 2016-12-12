e328be644f28a12f7764b955c365935eb900a1e1
egorsh0 <egorsh@inbox.ru>
2016-12-12 23:20:16 +0300

    Временный коммит файлов для сохранения

---
+++ Bookvoed_WPF/Bookvoed/Bookvoed/ViewModels/BookContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Bookvoed
+{
+    class BookContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
